441465c529789d6a298a85ac77534ade2e76e4d7
Charles Lowell <cowboyd@frontside.io>
2015-03-01 13:56:29 -0600

    include child results in validation

--- addon/models/rule.js
+++ addon/models/rule.js
@@ -4,8 +4,11 @@ import { RSVP, makePromiseObject } from '../utils/make-promise';
 export var RuleSet = Ember.Object.extend({
   form: null,
   definition: {},
-  result: Ember.computed('rules.@each.isPending', function() {
-    return makePromiseObject(RSVP.all(this.get('rules').mapBy('result')));
+  result: Ember.computed('rules.@each.result', 'children.@each.result', function() {
+    return makePromiseObject(RSVP.hash({
+      self: RSVP.all(this.get('rules').mapBy('result')),
+      children: RSVP.all(this.get('children').mapBy('result'))
+    }));
   }),
   ruleNames: Ember.computed('definition', function() {
     return Object.keys(this.get('definition'));
@@ -14,9 +17,9 @@ export var RuleSet = Ember.Object.extend({
     var Rule = Ember.Object.extend({
       form: this.get('form'),
       name: ruleName,
-      result: this.get('definition')[ruleName],
-      isPending: Ember.computed.reads('result.isPending')
+      result: this.get('definition')[ruleName]
     });
     return Rule.create();
-  })
+  }),
+  children: Ember.computed.mapBy('form._children', 'ruleSet')
 });
