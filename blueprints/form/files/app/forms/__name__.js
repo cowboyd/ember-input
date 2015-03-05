61394d730a71199013b86da05bdbdaac8231ab8b
Charles Lowell <cowboyd@frontside.io>
2015-03-05 04:28:40 -0800

    add blueprint for form

---
+++ blueprints/form/files/app/forms/__name__.js
@@ -0,0 +1,18 @@
+import Form from 'ember-input';
+
+export default Form.extend({
+
+  //form attributes go here.
+  //e.g.
+  // firstName: Form.hasOne(),
+  // lastName: Form.hasOne()
+
+  // validation rules go here. e.g.
+  //rules: {
+    // hasFullName: Form.rule('firstName.value', 'lastName.value', function() {
+    //   var first = this.get('firstName.value');
+    //   var last = this.get('lastName.value');
+    //   return !Ember.isEmpty(first) && !Ember.isEmpty(last);
+    // })
+  //}
+});
