5c99b44a7220c365e6a29dca69e00d6a6a78573a
Charles Lowell <cowboyd@frontside.io>
2015-03-10 11:43:29 -0500

    update form blueprint

--- blueprints/form/files/app/forms/__name__.js
+++ blueprints/form/files/app/forms/__name__.js
@@ -1,15 +1,15 @@
-import Form from 'ember-input';
+import {Form, field, rule } from 'ember-input';
 
 export default Form.extend({
 
   //form attributes go here.
   //e.g.
-  // firstName: Form.hasOne(),
-  // lastName: Form.hasOne()
+  // firstName: field(),
+  // lastName: field()
 
   // validation rules go here. e.g.
   //rules: {
-    // hasFullName: Form.rule('firstName.value', 'lastName.value', function() {
+    // hasFullName: rule('firstName.value', 'lastName.value', function() {
     //   var first = this.get('firstName.value');
     //   var last = this.get('lastName.value');
     //   return !Ember.isEmpty(first) && !Ember.isEmpty(last);
